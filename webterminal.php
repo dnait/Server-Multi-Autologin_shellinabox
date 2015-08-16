78cb58b9aa3d943581bad6962c28284524b6c718
Rao Katie Xu <grace@Graces-MacBook-Pro.local>
2015-08-16 01:53:40 -0400

    shellinabox auto login
    A detailed case with protocol on how to install the shellinabox and
    test auto login on a local machine. Work very well.

---
+++ webterminal.php
@@ -0,0 +1,51 @@
+<?php
+?>
+<html>
+       <table width="1350" height="400" border="1" align="center" cellspacing="0" bordercolor="#909090" style="border-collapse:collapse;" >
+           <tr><td colspan="2" height="60" bgcolor="#FFFFcc"><div align="center">
+                <div align='center'><strong>Welcome to UT Virtual Network Lab </strong></div>
+            </td>
+           
+        </tr>
+        <tr>
+            <td width="620" height="300">
+                      <input type="button" onclick="frame1.location.href='http://localhost:4200/?u=rxu2&p=1234abcd';document.getElementById('frame1').frameBorder=1" value="DefaultGW">
+<input type="button" onclick="frame1.location.href='blank.html';document.getElementById('frame1').frameBorder=0" value="Clear">
+<iframe src="blank.html" frameborder="0" name="frame1" id="frame1" width=620 height=300></iframe>
+
+               <td width="620" height="320">
+    <input type="button" onclick="frame2.location.href='http://localhost:4200/?u=rxu2&p=1234abcd';document.getElementById('frame2').frameBorder=1" value="ExtFW">
+<input type="button" onclick="frame2.location.href='blank.html';document.getElementById('frame2').frameBorder=0" value="Clear">
+<iframe src="blank.html" frameborder="0" name="frame2" id="frame2" width=620 height=300></iframe>
+       
+            </td>
+            </tr>
+            <tr><td>
+                
+                    <input type="button" onclick="frame3.location.href='http://localhost:4200/?u=rxu2&p=1234abcd';document.getElementById('frame3').frameBorder=1" value="Gemini">
+<input type="button" onclick="frame3.location.href='blank.html';document.getElementById('frame3').frameBorder=0" value="Clear">
+<iframe src="blank.html" frameborder="0" name="frame3" id="frame3" width=620 height=300></iframe>
+
+                </td><td> <input type="button" onclick="frame4.location.href='http://localhost:4200/?u=rxu2&p=1234abcd';document.getElementById('frame4').frameBorder=1" value="NFS">
+<input type="button" onclick="frame4.location.href='blank.html';document.getElementById('frame4').frameBorder=0" value="Clear">
+<iframe src="blank.html" frameborder="0" name="frame4" id="frame4" width=620 height=300></iframe>
+
+                    
+                </td>
+            </tr>
+             <tr><td>
+                
+                    <input type="button" onclick="frame5.location.href='http://localhost:4200/?u=rxu2&p=1234abcd';document.getElementById('frame5').frameBorder=1" value="IntFW">
+<input type="button" onclick="frame5.location.href='blank.html';document.getElementById('frame5').frameBorder=0" value="Clear">
+<iframe src="blank.html" frameborder="0" name="frame5" id="frame5" width=620 height=300></iframe>
+
+                </td><td> <input type="button" onclick="frame6.location.href='http://localhost:4200/?u=rxu2&p=1234abcd';document.getElementById('frame6').frameBorder=1" value="DMZ">
+<input type="button" onclick="frame6.location.href='blank.html';document.getElementById('frame6').frameBorder=0" value="Clear">
+<iframe src="blank.html" frameborder="0" name="frame6" id="frame6" width=620 height=300></iframe>
+             </tr>
+            
+          
+       </table>
+    
+</html>
+
